7495af00ae07dd34d6b2b44d30fc5b294aae605c
Alberto Herrera <albertoherreravargas@gmail.com>
2018-10-13 13:41:06 +0200

    Updating index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const paciente = require('./controllers/models/paciente')
+const doctor = require('./controllers/models/doctor')
+const cita = require('./controllers/models/cita')
 
-var nuevopaciente= new paciente("alberto","herrera", 123123123);
 
-console.log(nuevopaciente.getApellido());
+var nuevopaciente= new paciente("Alberto","Herrera", 123123123);
+var nuevodoctor= new doctor("Jose","Alcantara", 123123123, "Traumatologo");
+var nuevacita= new cita(nuevopaciente,nuevodoctor,"12/12/1999", "12:00", 1);
+
+console.log(nuevopaciente.toString());
+console.log(nuevodoctor.toString());
+console.log(nuevacita.toString());
  
