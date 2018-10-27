1fb6d7c6275c132ad297a6df940eb2d8ddc68c85
Alberto Herrera <albertoherreravargas@gmail.com>
2018-10-27 19:55:30 +0200

    Deploying on Heroku with status OK

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const paciente = require('./controllers/models/paciente')
 const doctor = require('./controllers/models/doctor')
 const cita = require('./controllers/models/cita')
+const resultado = require('./result.json')
 var express = require('express');
 var app = express();
 
@@ -9,22 +10,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.send('Hola Alber!');
-  });
-
-
-
-
-
-var nuevopaciente= new paciente("Alberto","Herrera", 123123123);
-var nuevodoctor= new doctor("Jose","Alcantara", 123123123, "Traumatologo");
-var nuevacita= new cita(nuevopaciente,nuevodoctor,"12/12/1999", "12:00", 1);
-
-console.log(nuevopaciente.toString());
-console.log(nuevodoctor.toString());
-console.log(nuevacita.toString());
+    res.json({"status": "OK"});
+})
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
   });
- 
