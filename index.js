991b2a616ef9117a01ee850438f760ac05cf5371
Alberto Herrera <albertoherreravargas@gmail.com>
2018-10-27 19:40:18 +0200

    Modifying  index and package

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const paciente = require('./controllers/models/paciente')
 const doctor = require('./controllers/models/doctor')
 const cita = require('./controllers/models/cita')
+var express = require('express');
+var app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.send('Hola Alber!');
+  });
+
+
+
 
 
 var nuevopaciente= new paciente("Alberto","Herrera", 123123123);
@@ -10,4 +23,8 @@ var nuevacita= new cita(nuevopaciente,nuevodoctor,"12/12/1999", "12:00", 1);
 console.log(nuevopaciente.toString());
 console.log(nuevodoctor.toString());
 console.log(nuevacita.toString());
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+  });
  
