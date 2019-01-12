e99bfbaaa6fb76d66b23d2343fc6e13e1100efdf
Alberto Herrera <albertoherreravargas@gmail.com>
2019-01-12 18:22:49 +0100

    Adding Flightplan file closes #22

--- despliegue/flightplan.js
+++ despliegue/flightplan.js
@@ -1,17 +1,17 @@
 var plan = require('flightplan');
 
 plan.target('staging', {
-  host: 'maquinavagrant.westeurope.cloudapp.azure.com',
+  host: 'vmazure.westus.cloudapp.azure.com',
   username: 'vagrant',
   agent: process.env.SSH_AUTH_SOCK
 });
 
 plan.remote(function(remote) {
 
-  remote.with('cd /opt/Hospital/', function() {
+  
     remote.exec('sudo npm install --production');
     remote.exec('sudo forever start index.js');
-  });
+
 
 });
 
